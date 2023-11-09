13de2ef508aeeef3f97f7c6e8185008f07986b61
wendyprogramic <w.dekock@programic.com>
2023-11-09 14:42:53 +0100

    [exercises] excersize 01

---
+++ 01/01_start.js
@@ -0,0 +1,14 @@
+// Create a variable that holds your full name as a string
+const fullName = 'Wendy de Kock';
+
+// Replace your last name with 'the js master'
+/** The replace function does not mutate the original  */
+const title = fullName.replace('de Kock', 'the JS master');
+
+// In the log function, log out 'Hi' and then the variable that holds the replaced message
+console.log(title);
+
+// Create a new variable that uses the title variable and creates an array that looks like this [ 'Wendy', 'the', 'JS', 'master' ]
+/** Seperate the string on spaces */
+
+// log out the word 'JS' in the console
